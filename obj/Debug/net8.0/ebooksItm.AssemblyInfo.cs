6d0e549c72eb843880c07fb4ffe2f65b9e84d720
diego1286 <diego.ardilas2486@gmail.com>
2024-05-11 07:08:05 -0500

    ficheros docker

--- obj/Debug/net8.0/ebooksItm.AssemblyInfo.cs
+++ obj/Debug/net8.0/ebooksItm.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ebooksItm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d215513b1730404f04a63d6b2b646324a2a95ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+342d4d7d39edfbaf4b58528ecd02f4ceeed1a3a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ebooksItm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ebooksItm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
